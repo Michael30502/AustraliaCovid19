07b50b9dcbd432d54e2023b2ba2e9213ae0f1af4
Michael Bendtsen <mich.bendtsen@gmail.com>
2020-10-29 10:59:16 +0100

    Setup

--- src/main/java/AustraliaCorona.java
+++ src/main/java/AustraliaCorona.java
@@ -1,10 +1,20 @@
 import processing.core.PApplet;
 
-public class AustraliaCorona {
+public class AustraliaCorona extends PApplet {
 
     public static void main(String[] args ) {
         PApplet.main("name");
     }
 
+
+    public void settings() {
+        size(1792,828);
+    }
+
+    public void setup(){
+        
+
+
+    }
 
 }
